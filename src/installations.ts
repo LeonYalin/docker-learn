b332f78a6c2cfdbb61f7f352684282a60d552dc5
Leon Yalin <leon.yalin@cellebrite.com>
2020-02-19 12:17:20 +0200

    basic project template

---
+++ src/installations.ts
@@ -0,0 +1,10 @@
+import { delimeterMsg, logF } from "./utils";
+
+function installingDocker() {
+  // TODO
+}
+
+export default function installations() {
+  delimeterMsg('INSTALLATIONS');
+  logF(installingDocker);
+}
