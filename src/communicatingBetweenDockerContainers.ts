ccffb492459ec91cc4931dfd051903af6f2bfac0
Leon Yalin <yelinichevl@gmail.com>
2020-03-07 13:30:21 +0200

    add communicating between docker containers

---
+++ src/communicatingBetweenDockerContainers.ts
@@ -0,0 +1,46 @@
+import { delimeterMsg, logF, logToHTML } from "./utils";
+
+function usingLegacylinking() {
+  logToHTML('' +
+    'Docker containers can be likned to each other using two methods: legacy linking and container networks. First method is more ' +
+    'simple but is depricated and will be removed soon. In this method we link containers by name. When we start a container we give it a name. ' +
+    'e.g. "docker run -d --name my-mongodb mongo". Then, we\'ll run the next container and link it to first using the name we created earlier. ' +
+    'e.g. "docker run -d -p 80:3000 --link my-mongodb:mongodb node". In the --link command the first argument is a name and second is an alias. ' +
+    'This alias can be used later inside the container as a host name to connect to the first container, e.g. in mongodb "host" configuration. ' +
+    'In order to test it, we will run the node container with mongo container linked, then enter the node container and try to run mongo commands. ' +
+    `
+      docker run -d --name mongodb mongo
+      docker run -d --name nodejs --link mongodb:mongodb leonyalin/node
+    ` +
+    'To check if it is worked we can enter the container and ping the linked address, or view the hosts file, like this:' +
+    `
+      docker exec -it 1e sh
+      cat /etc/hosts
+      ping mongodb
+    ` +
+    'One more way to run command in the running container is "docker exec <container_id> [commands]", e.g. docker exec 1e node server.js'
+  );
+}
+
+function usingContainerNetworks() {
+  logToHTML('' +
+    'Linking containers is good in development, but bad in production when you have dosens og containers that need to talk to each other. ' +
+    'Docker solves this problem by using container networks, so called "bridge networks". We can can create a container network ' +
+    'and then run containers inside that network. All containers inside te network will be able to talk to each other. ' +
+    'First, we create a network using the command "docker network create --driver bridge <network_name>". We can wiew it using "docker network ls"' +
+    'Then, we run containers and provide an additional --net=<network_name> to connect to the network. ' +
+    'To check which containers run inside the network, we can type "docker network inspect <network_name>' +
+    '' +
+    'Additional example for cmd: "docker run -it -p 80:3000 -v %cd%:/var/www -w /var/www node sh".' +
+    'This will link the current directory to the container, set it as current directory (using the -w option) and run the "npm start" command.' +
+    'And we don\'t have the nodejs to be installed our our machine! Think about some complex installations, like APS.NET or JAVA...' +
+    'One more use case is to develop locally and to test the STAGING/PRODUCTION environments on docker containers.' +
+    ''
+  );
+}
+
+export default function communicatingBetweenDockerContainers() {
+  delimeterMsg('COMMUNICATING BETWEEN DOCKER CONTAINERS');
+  logF(usingLegacylinking);
+  logF(usingContainerNetworks);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import installations from "./installations";
 import usingDockerTools from "./usingDockerTools";
 import hookingSourceCodeIntoContainer from "./hookingSourceCodeIntoContainer";
+import communicatingBetweenDockerContainers from "./communicatingBetweenDockerContainers";
 
 (function main() {
   installations();
   usingDockerTools();
   hookingSourceCodeIntoContainer();
+  communicatingBetweenDockerContainers();
 }());
